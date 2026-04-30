b59fc7b2dd068e65b6b8d77b26b0d47a5ef15063
ofealves <131112250+ofealves@users.noreply.github.com>
2026-04-30 00:53:46 -0300

    fix: return user on login

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -28,14 +28,20 @@ export const loginUser = async (req: Request, res: Response) => {
         const Users = await User.findOne({
             email
         })
-        if(!Users) return res.status(404).json({ error: 'Usuario nao encontrado'});
-        const hashed = await bcrypt.compare(password, Users.password );
-        if(!hashed) return res.status(401).json({ error: 'Dados invalidos'});
+        if (!Users) return res.status(404).json({ error: 'Usuario nao encontrado' });
+        const hashed = await bcrypt.compare(password, Users.password);
+        if (!hashed) return res.status(401).json({ error: 'Dados invalidos' });
         console.log("JWT_SECRET:", process.env.JWT_SECRET);
-        const token = jwt.sign({ _id: Users._id, role: Users.role }, process.env.JWT_SECRET as string, { expiresIn: "1d" } );
-        return res.json({ token })
-    } catch(error) {
-    console.error("[loginUser]", error);
-    return res.status(500).json({ error: "Erro ao criar Usuario" });
-}
+        const token = jwt.sign({ _id: Users._id, role: Users.role }, process.env.JWT_SECRET as string, { expiresIn: "1d" });
+        return res.json({
+            token,
+            user: {
+                name: Users.name,
+                role: Users.role
+            }
+        });
+    } catch (error) {
+        console.error("[loginUser]", error);
+        return res.status(500).json({ error: "Erro ao criar Usuario" });
+    }
 }
