335a1c15a137956c410d5631b7ae3b17d96f54c3
ofealves <131112250+ofealves@users.noreply.github.com>
2026-05-08 12:09:06 -0300

    cada usuario ve somente o seu tickets

--- src/modules/tickets/ticket.controller.ts
+++ src/modules/tickets/ticket.controller.ts
@@ -4,6 +4,7 @@ import Ticket from "./ticket.model";
 export const createTicket = async (req: Request, res: Response) => {
     try {
         const { title, description, priority } = req.body;
+
         const userId = req.user?._id;
 
         const ticket = await Ticket.create({
@@ -19,10 +20,24 @@ export const createTicket = async (req: Request, res: Response) => {
     }
 };
 
-export const getTickets = async (_req: Request, res: Response) => {
+export const getTickets = async (req: Request, res: Response) => {
     try {
-        const tickets = await Ticket.find().sort({ createdAt: -1 });
-        return res.json(tickets);
+        const userId = req.user?._id;
+        const role = req.user?.role;
+
+        let tickets;
+
+        if (role === "admin") {
+            tickets = await Ticket.find({ userId } as any).sort({
+                createdAt: -1,
+            });
+        } else {
+            tickets = await Ticket.find({ userId } as any).sort({
+                createdAt: -1,
+            });
+        }
+
+        return res.status(200).json(tickets);
     } catch (error) {
         return res.status(500).json({ error: "Erro ao buscar tickets" });
     }
@@ -30,58 +45,111 @@ export const getTickets = async (_req: Request, res: Response) => {
 
 export const getTicketById = async (req: Request, res: Response) => {
     try {
-        console.log("🔥 bateu no getTicketById");
-        console.log("ID:", req.params.id);
+        const userId = req.user?._id;
+        const role = req.user?.role;
 
-        const ticket = await Ticket.findOne({ _id: req.params.id } as any);
+        let ticket;
 
-        console.log("RESULT:", ticket);
+        if (role === "admin") {
+            ticket = await Ticket.findOne({
+                _id: req.params.id,
+            } as any);
+        } else {
+            ticket = await Ticket.findOne({
+                _id: req.params.id,
+                userId,
+            } as any);
+        }
 
         if (!ticket) {
-            return res.status(404).json({ error: "Ticket não encontrado" });
+            return res.status(404).json({
+                error: "Ticket não encontrado",
+            });
         }
 
         return res.status(200).json(ticket);
     } catch (error) {
-        console.log("ERRO REAL:", error);
-        return res.status(500).json({ error: "Erro ao buscar ticket" });
+        return res.status(500).json({
+            error: "Erro ao buscar ticket",
+        });
     }
 };
 
-
-
 export const updateTicket = async (req: Request, res: Response) => {
     try {
-        const TicketId = req.params.id;
+        const userId = req.user?._id;
+        const role = req.user?.role;
+
+        const ticketId = req.params.id;
         const { status } = req.body;
 
-        const updated = await Ticket.updateOne(
-            { _id: TicketId },
-            { status }
-        );
+        let updated;
 
-        if (updated.matchedCount === 0) {
-            return res.status(404).json({ error: "Ticket não encontrado" });
+        if (role === "admin") {
+            updated = await Ticket.updateOne(
+                { _id: ticketId },
+                { status }
+            );
+        } else {
+            updated = await Ticket.updateOne(
+                {
+                    _id: ticketId,
+                    userId,
+                },
+                {
+                    status,
+                }
+            );
         }
 
-        return res.status(200).json(updated);
+        if (updated.matchedCount === 0) {
+            return res.status(404).json({
+                error: "Ticket não encontrado",
+            });
+        }
+
+        return res.status(200).json({
+            message: "Ticket atualizado",
+        });
     } catch (error) {
-        return res.status(500).json({ error: "Erro ao atualizar documento" });
+        return res.status(500).json({
+            error: "Erro ao atualizar ticket",
+        });
     }
 };
 
 export const deleteTicket = async (req: Request, res: Response) => {
     try {
-        const TicketId = req.params.id;
+        const userId = req.user?._id;
+        const role = req.user?.role;
 
-        const deleted = await Ticket.deleteOne({ _id: TicketId });
+        const ticketId = req.params.id;
 
-        if (deleted.deletedCount === 0) {
-            return res.status(404).json({ error: "Ticket não encontrado" });
+        let deleted;
+
+        if (role === "admin") {
+            deleted = await Ticket.deleteOne({
+                _id: ticketId,
+            });
+        } else {
+            deleted = await Ticket.deleteOne({
+                _id: ticketId,
+                userId,
+            });
         }
 
-        return res.status(200).json({ message: "Ticket deletado" });
+        if (deleted.deletedCount === 0) {
+            return res.status(404).json({
+                error: "Ticket não encontrado",
+            });
+        }
+
+        return res.status(200).json({
+            message: "Ticket deletado",
+        });
     } catch (error) {
-        return res.status(500).json({ error: "Erro ao deletar documento" });
+        return res.status(500).json({
+            error: "Erro ao deletar ticket",
+        });
     }
 };
